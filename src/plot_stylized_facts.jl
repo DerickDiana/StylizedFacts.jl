cd2e3d7a801b6da07d0f7948449cdb8d5a18ee5d
Michael Gant <michal.gant@baringa.com>
2021-10-02 21:27:15 +0100

    adding titles to all plots

--- src/plot_stylized_facts.jl
+++ src/plot_stylized_facts.jl
@@ -1,11 +1,11 @@
 mutable struct StylizedFactsPlot
-    price_path::Array{Float64, 1}
-    log_price_path::Array{Float64, 1}
-    log_returns::Array{Float64, 1}
-    order_signs::Array{Float64, 1}
-    order_flow_acf::Array{Float64, 1}
-    log_returns_acf::Array{Float64, 1}
-    abs_log_returns_acf::Array{Float64, 1}
+    price_path::Array{Float64,1}
+    log_price_path::Array{Float64,1}
+    log_returns::Array{Float64,1}
+    order_signs::Array{Float64,1}
+    order_flow_acf::Array{Float64,1}
+    log_returns_acf::Array{Float64,1}
+    abs_log_returns_acf::Array{Float64,1}
     N::Int
     L::Int
     l_conf::Float64
@@ -24,8 +24,8 @@ function StylizedFactsPlot(price_path)
 
     N = size(price_path, 1)
     L = size(log_returns_acf, 1)
-    l_conf = -1.96/sqrt(N)
-    u_conf = 1.96/sqrt(N)
+    l_conf = -1.96 / sqrt(N)
+    u_conf = 1.96 / sqrt(N)
 
     return StylizedFactsPlot(price_path, log_price_path, log_returns,
         order_signs, order_flow_acf, log_returns_acf, abs_log_returns_acf,
@@ -33,8 +33,8 @@ function StylizedFactsPlot(price_path)
 end
 
 function hist_log_returns(sf::StylizedFactsPlot)
-    pq = histogram(sf.log_returns, normalize=:pdf, label = "observed returns",
-        ylab = "Density", xlab="Return");
+    pq = histogram(sf.log_returns, normalize=:pdf, label="observed returns",
+        ylab="Density", xlab="Return", title="Log Returns Histogram");
     f(x) = pdf(fit(Normal, sf.log_returns), x)
     plot!(pq, f, label="Normal Distribution");
     return pq
@@ -42,7 +42,7 @@ end
 
 function plot_log_returns(sf::StylizedFactsPlot)
     p1 = plot(1:size(sf.log_returns, 1), sf.log_returns, legend=false,
-    xlab = "Time", ylab = "Log Returns");
+    xlab="Time", ylab="Log Returns", title="Log Returns");
     return p1
 end
 
@@ -54,44 +54,44 @@ end
 
 function plot_acf_log_returns(sf::StylizedFactsPlot)
 
-    p1 = plot(1:sf.L, sf.log_returns_acf, xlab = "Lag",
-        ylab = "ACF", title="Log Returns Autocorrelation",
+    p1 = plot(1:sf.L, sf.log_returns_acf, xlab="Lag",
+        ylab="ACF", title="Log Returns Autocorrelation",
         legend=false, seriestype=:sticks);
-    plot!(p1, 1:sf.L, x->0, linestyle = :solid, color="black")
-    plot!(p1, 1:sf.L, x->sf.l_conf, linestyle = :dot)
-    plot!(p1, 1:sf.L, x->sf.u_conf, linestyle = :dot)
+    plot!(p1, 1:sf.L, x -> 0, linestyle=:solid, color="black")
+    plot!(p1, 1:sf.L, x -> sf.l_conf, linestyle=:dot)
+    plot!(p1, 1:sf.L, x -> sf.u_conf, linestyle=:dot)
 
     return p1
 end
 
 function plot_acf_order_flow(sf::StylizedFactsPlot)
 
-    p1 = plot(1:sf.L, sf.order_flow_acf, xlab = "Lag",
-        ylab = "ACF", title="Order Flow Autocorrelation (Tick Rule)",
+    p1 = plot(1:sf.L, sf.order_flow_acf, xlab="Lag",
+        ylab="ACF", title="Order Flow Autocorrelation (Tick Rule)",
         legend=false, seriestype=:sticks);
-    plot!(p1, 1:sf.L, x->0, linestyle = :solid, color="black")
-    plot!(p1, 1:sf.L, x->sf.l_conf, linestyle = :dot)
-    plot!(p1, 1:sf.L, x->sf.u_conf, linestyle = :dot)
+    plot!(p1, 1:sf.L, x -> 0, linestyle=:solid, color="black")
+    plot!(p1, 1:sf.L, x -> sf.l_conf, linestyle=:dot)
+    plot!(p1, 1:sf.L, x -> sf.u_conf, linestyle=:dot)
     return p1
 end
 
 function plot_acf_abs_log_returns(sf::StylizedFactsPlot)
 
-    p1 = plot(1:sf.L, sf.abs_log_returns_acf, xlab = "Lag",
-        ylab = "ACF", title="Absolute Log Returns Autocorrelation",
+    p1 = plot(1:sf.L, sf.abs_log_returns_acf, xlab="Lag",
+        ylab="ACF", title="Absolute Log Returns Autocorrelation",
         legend=false, seriestype=:sticks);
-    plot!(p1, 1:sf.L, x->0, linestyle = :solid, color="black")
-    plot!(p1, 1:sf.L, x->sf.l_conf, linestyle = :dot)
-    plot!(p1, 1:sf.L, x->sf.u_conf, linestyle = :dot)
+    plot!(p1, 1:sf.L, x -> 0, linestyle=:solid, color="black")
+    plot!(p1, 1:sf.L, x -> sf.l_conf, linestyle=:dot)
+    plot!(p1, 1:sf.L, x -> sf.u_conf, linestyle=:dot)
     return p1
 end
 
-function plot_all_stylized_facts(sf::StylizedFactsPlot, plot_size=(1000,800))
+function plot_all_stylized_facts(sf::StylizedFactsPlot, plot_size=(1000, 800))
 
     l = @layout [a b ; c d ; e f]
 
     p1 = plot(1:size(sf.price_path, 1), sf.price_path, legend=false,
-    xlab = "Time", ylab = "Price");
+    xlab="Time", ylab="Price", title="Mid-Price Path");
     p2 = plot_log_returns(sf)
     p3 = plot_qq_log_returns(sf)
     p4 = plot_acf_order_flow(sf)
